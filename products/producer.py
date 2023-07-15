4ded69e6098acd9ff7d4bce3c9be4c04b3fee23a
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-15 06:49:57 -0700

    data consistency and django app completed

--- products/producer.py
+++ products/producer.py
@@ -1,7 +1,7 @@
 
 
 
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://icmnwovy:ZSZFR3cURlyYM0WXYW2TVvquwgnwhF77@puffin.rmq2.cloudamqp.com/icmnwovy')
 
@@ -9,5 +9,6 @@ connection = pika.BlockingConnection(params)
 channel= connection.channel()
 
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='Hello jan')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
