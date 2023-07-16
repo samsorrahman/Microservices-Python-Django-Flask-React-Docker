6fcf09f195dd441e31c81286873d4f25f04c2cd6
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-16 14:09:22 +0500

    Add files via upload

---
+++ main-flask/producer.py
@@ -0,0 +1,14 @@
+
+
+
+import pika, json
+
+params = pika.URLParameters('amqps://icmnwovy:ZSZFR3cURlyYM0WXYW2TVvquwgnwhF77@puffin.rmq2.cloudamqp.com/icmnwovy')
+
+connection = pika.BlockingConnection(params)
+channel= connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
