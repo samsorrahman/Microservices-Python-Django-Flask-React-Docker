712ef1f055b9f50051db2932381f6c2eb63bd401
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-15 22:57:11 -0700

    like functionality accros the app

--- consumer.py
+++ consumer.py
@@ -1,7 +1,12 @@
 
 
+import pika, json, os, django
 
-import pika
+# the consumer file give error becaue we are using product class but consumer is outside the product project so we add this
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
 
 params = pika.URLParameters('amqps://icmnwovy:ZSZFR3cURlyYM0WXYW2TVvquwgnwhF77@puffin.rmq2.cloudamqp.com/icmnwovy')
 
@@ -12,7 +17,12 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
     print('Recieved in admin')
-    print(body)
+    id= json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased!')
 
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
--- products/views.py
+++ products/views.py
@@ -13,7 +13,6 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
         products= Product.objects.all()
         serializer= ProductSerializer(products, many=True)
-        publish()
         return Response(serializer.data)
     
     def create(self, request):
